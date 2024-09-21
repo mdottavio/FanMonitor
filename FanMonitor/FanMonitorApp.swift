1a34aaf9d29f229a67e331a21a5be068dabd7715
Mauricio Dottavio <dottavio@gmail.com>
2024-09-21 13:27:55 -0300

    Initial Commit

---
+++ FanMonitor/FanMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FanMonitorApp.swift
+//  FanMonitor
+//
+//  Created by Mauricio Dottavio on 21/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct FanMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FanMonitorTests/FanMonitorTests.swift
@@ -0,0 +1,17 @@
+//
+//  FanMonitorTests.swift
+//  FanMonitorTests
+//
+//  Created by Mauricio Dottavio on 21/09/2024.
+//
+
+import Testing
+@testable import FanMonitor
+
+struct FanMonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
